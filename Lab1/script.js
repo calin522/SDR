59341e1b2cbd985613ce33d3a4b14128a21cdf44
Calin Ardeleanu <calinardeleanu@Calins-MacBook-Air.local>
2025-10-14 20:07:53 +0300

    Test

--- Lab1/script.js
+++ Lab1/script.js
@@ -1,1 +1,16 @@
 // id,movie_name,rating,runtime,genre,metascore,plot,directors,stars,votes,gross
+
+import fs from 'fs';
+import csv from 'csv-parser';
+import token from './data/token.json' assert { type: 'json' };
+
+const recombee = require('recombee-api-client');
+
+const client = new recombee.ApiClient(
+  'politehnica-dev',  
+  token.tokenId,
+// Show public token
+{ 'region': 'eu-west' }
+);
+
+// console.log(client._getBaseUri());
